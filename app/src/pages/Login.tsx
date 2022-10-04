cc7cf61c330d0f7cc901337e637682932a33aba3
Abdallah Ziad Al Naif <Abdallahalnaif@hotmail.com>
2022-10-04 10:25:27 +0200

    Content Creator form made
    Content Creator form made including error handling, validation, and a post request to the server.

--- app/src/pages/Login.tsx
+++ app/src/pages/Login.tsx
@@ -9,16 +9,16 @@ const Login = () => {
                 <div className='flex flex-col divide text-gray-700'>
                     <form className="flex flex-col" action="">
                         <label className="font-semibold text-xs" htmlFor="usernameField">Email</label>
-                        <input className="flex items-center h-12 px-4 w-64 bg-gray-200 mt-2 rounded focus:outline-none focus:ring-2" type="text" />
+                        <input className="flex items-center h-12 px-4 w-64 bg-gray-100 mt-2 rounded focus:outline-none focus:ring-2" type="text" />
                         <label className="font-semibold text-xs mt-3" htmlFor="passwordField">Password</label>
-                        <input className="flex items-center h-12 px-4 w-64 bg-gray-200 mt-2 rounded focus:outline-none focus:ring-2" type="password" />
+                        <input className="flex items-center h-12 px-4 w-64 bg-gray-100 mt-2 rounded focus:outline-none focus:ring-2" type="password" />
                         <button className="flex items-center justify-center h-12 px-6 w-64 bg-blue-600 mt-8 rounded font-semibold text-sm text-blue-100 hover:bg-blue-700">
                             Login
                         </button>
                     </form>
 
                     <button className="flex items-center justify-center h-12 px-6 w-64 mt-4 rounded font-semibold text-sm bg-white border border-blue-500 shadow-md hover:border-blue-700">
-                        <img alt="google icon" src="https://developers.google.com/identity/sign-in/g-normal.png" className='mr-2 h-8 w-8'/>
+                        <img alt="google icon" src="https://developers.google.com/identity/sign-in/g-normal.png" className='mr-2 h-8 w-8' />
                         Sign in with Google
                     </button>
                 </div>
