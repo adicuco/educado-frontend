149ad4c38790455c59a4d575c4abb650f253af65
bodeby <thorbensen@gmail.com>
2022-11-08 10:12:31 +0100

    feat:app section list

--- app/src/components/dnd/SectionList.tsx
+++ app/src/components/dnd/SectionList.tsx
@@ -1,5 +1,7 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
+import { useForm, SubmitHandler } from 'react-hook-form';
 
+// DND-KIT
 import {
   DndContext,
   closestCenter,
@@ -24,29 +26,25 @@ import {
 // Components
 import { SortableItem } from './@dnd/SortableItem';
 import { Item } from './@dnd/Item';
+import { PlusIcon } from '@heroicons/react/24/outline';
 
-const exampleCourse = {
-  "sections": [
-    "How to download Rush Royale",
-    "How to play CO-OP",
-    "How to play PVP"
-  ],
-  "_id": "6228653fee41451cb9640a82",
-  "title": "MWUHAHAHA",
-  "description": "WORK U BITCH",
-  "category": "",
-  "_user": "620e525dbc5384023a2ce500",
-  "dateCreated": "2022-03-09T08:28:47.440Z",
-  "dateUpdated": "2022-03-09T08:28:47.440Z",
-  "__v": 2,
-  "published": true,
-  "coverImg": "1648541890581-mustache.png"
-};
+// Interfaces
+type Inputs = {
+  input: string
+}
+
 
 export const SectionList = ({ sections }: { sections: Array<string> }) => {
-  const [activeId, setActiveId] = useState(null);
-  const [items, setItems] = useState(exampleCourse.sections);
+  // React useForm setup
+  const { register, handleSubmit, formState: { errors } } = useForm<Inputs>();
+  const onSubmit: SubmitHandler<Inputs> = (data) => { console.log(data); }
 
+  // States
+  const [activeId, setActiveId] = useState(null);
+  const [items, setItems] = useState(sections);
+
+
+  // Setup of pointer and keyboard sensor
   const sensors = useSensors(
     useSensor(PointerSensor),
     useSensor(KeyboardSensor, {
@@ -54,12 +52,14 @@ export const SectionList = ({ sections }: { sections: Array<string> }) => {
     })
   );
 
-  function handleDragStart(event: any) {
+  // handle start of dragging
+  const handleDragStart = (event: any) => {
     const { active } = event;
     setActiveId(active.id);
   }
 
-  function handleDragEnd(event: any) {
+  // handle end of dragging
+  const handleDragEnd = (event: any) => {
     const { active, over } = event;
     if (active.id !== over.id) {
       setItems((items) => {
@@ -88,6 +88,24 @@ export const SectionList = ({ sections }: { sections: Array<string> }) => {
           {activeId ? <Item id={activeId} /> : null}
         </DragOverlay>
       </DndContext>
+
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="flex justify-between items-center border rounded p-2">
+          <div >
+            <button type="submit" className='btn btn-ghost'>
+              <PlusIcon width={24} />
+            </button>
+          </div>
+
+          <div className='flex justify-between w-full space-x-2 ml-2'>
+            <label htmlFor='title' className='hidden'>Add New</label>
+            <input type="text" defaultValue={"Add new"}
+              className="form-field focus:outline-none focus:ring-2 focus:ring-gray-200 focus:border-transparent border-0 w-full shadow-none"
+              {...register("input", { required: false })}
+            />
+          </div>
+        </div>
+      </form>
     </div>
   );
 }
