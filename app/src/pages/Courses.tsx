f4aa45e9ad6bf768b193b22ed1e3ae02d6a5bac9
adicuco <cucolas.adi@gmail.com>
2023-09-14 13:55:11 +0200

    ci(github): add typecheck action

--- app/src/pages/Courses.tsx
+++ app/src/pages/Courses.tsx
@@ -1,19 +1,19 @@
-import useSWR from 'swr';
-import { useNavigate } from 'react-router-dom';
+import useSWR from "swr";
+import { useNavigate } from "react-router-dom";
 
-// Hooks 
-import useToken from '../hooks/useToken';
+// Hooks
+import useToken from "../hooks/useToken";
 
 // Services
-import CourseServices from '../services/course.services';
+import CourseServices from "../services/course.services";
 
 // Components
-import Layout from '../components/Layout'
-import Loading from './Loading';
-import { CourseListCard } from '../components/Courses/CourseListCard'
-import { CreateCourseModal } from '../components/Courses/CreateCourseModal';
-import { PageDescriptor } from '../components/PageDescriptor';
-import { CubeTransparentIcon } from '@heroicons/react/24/outline';
+import Layout from "../components/Layout";
+import Loading from "./Loading";
+import { CourseListCard } from "../components/Courses/CourseListCard";
+import { CreateCourseModal } from "../components/Courses/CreateCourseModal";
+import { PageDescriptor } from "../components/PageDescriptor";
+import { CubeTransparentIcon } from "@heroicons/react/24/outline";
 
 const Courses = () => {
   // States and Hooks
@@ -27,12 +27,11 @@ const Courses = () => {
   );
 
   // useSWR built in loaders
-  if (error) return navigate("/login");
-  if (!data) return <Loading/>;
+  // if (error) return navigate("/login");
+  if (!data) return <Loading />;
 
   return (
     <Layout meta="Course overview">
-
       {/** Page Descriptor */}
       <PageDescriptor
         title="Courses"
@@ -48,21 +47,27 @@ const Courses = () => {
       </div>
 
       {/** Page content real data from backend */}
-      {data.data.length ?
+      {data.data.length ? (
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-6 p-6">
-          {data.data.map((course: any, key: number) => <CourseListCard course={course} key={key} />)}
-        </div> :
-        <div className='flex flex-col space-y-8 justify-center items-center p-6'>
-          <CubeTransparentIcon width={44} className="lg:mt-24 text-primary"/>
-          <div className='flex flex-col text-center space-y-4'>
-            <h2 className='text-2xl'>Seems like you haven't created a course yet</h2>
-            <h2 className='text-sm'>You can use the 'CREATE NEW COURSE' button to get started</h2>
+          {data.data.map((course: any, key: number) => (
+            <CourseListCard course={course} key={key} />
+          ))}
+        </div>
+      ) : (
+        <div className="flex flex-col space-y-8 justify-center items-center p-6">
+          <CubeTransparentIcon width={44} className="lg:mt-24 text-primary" />
+          <div className="flex flex-col text-center space-y-4">
+            <h2 className="text-2xl">
+              Seems like you haven't created a course yet
+            </h2>
+            <h2 className="text-sm">
+              You can use the 'CREATE NEW COURSE' button to get started
+            </h2>
           </div>
         </div>
-      }
+      )}
     </Layout>
-  )
-}
+  );
+};
 
-export default Courses
-
+export default Courses;
